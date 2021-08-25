e19c51e12e8b9af957513d160c566fa118f4c3d9
kanav3745 <54405774+kanav3745@users.noreply.github.com>
2021-08-25 15:22:26 +0530

    Add files via upload

---
+++ TASK 4.sql
@@ -0,0 +1,22 @@
+use celebel;
+select con.contest_id,
+con.hacker_id,
+con.name,
+sum(total_submissions) as Total_submission,
+sum(total_accepted_submissions) as Total_accepted_submission,
+sum(total_views) as Total_views, sum(total_unique_views) as total_unique_views
+from contests con
+join colleges col on con.contest_id = col.contest_id
+join challenges cha on col.college_id = cha.college_id
+left join
+(select challenge_id , sum(total_views) as total_views, sum(total_unique_views) as total_unique_views
+from view_stats group by challenge_id) vs on cha.challenge_id=vs.challenge_id
+left join 
+(select challenge_id, sum(total_submissions) as total_submissions, sum(total_accepted_submissions) as total_accepted_submissions
+from Submission_stats group by challenge_id) ss on cha.challenge_id=ss.challenge_id
+group by con.contest_id,con.hacker_id,con.name
+having sum(total_submissions)!=0 or
+sum(total_accepted_submissions)!=0 or
+sum(Total_views)!=0 or
+sum(total_unique_views)!=0
+order by contest_id;
